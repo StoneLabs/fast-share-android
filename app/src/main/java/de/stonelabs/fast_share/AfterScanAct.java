3b64bf83c8cd3ec105701a478e032fdd82c3c1dc
Michael Holzmann <m.holzmann98@gmx.de>
2017-04-01 10:25:14 +0200

    Initial commit

---
+++ app/src/main/java/de/stonelabs/fast_share/AfterScanAct.java
@@ -0,0 +1,13 @@
+package de.stonelabs.fast_share;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AfterScanAct extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.afterscanact);
+    }
+}
