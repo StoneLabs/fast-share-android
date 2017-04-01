1cc8b420e486c2e3e3a7dcd9b92d5c1e379e344f
Stone <levyehrstein@googlemail.com>
2017-04-01 19:39:49 +0200

    Implemented share intend

---
+++ app/src/main/java/de/stonelabs/fastshare/share.java
@@ -0,0 +1,13 @@
+package de.stonelabs.fastshare;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class share extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_share);
+    }
+}
